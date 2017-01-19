d28fa310357de1193fc4d1f21620e926828af4a2
web6600 <web6600@caelum55-15>
2017-01-19 21:11:50 -0200

    19-01-2017

--- f21-agenda/src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
+++ f21-agenda/src/br/com/caelum/agenda/servlet/AdicionaContatoServlet.java
@@ -1,41 +1,54 @@
 package br.com.caelum.agenda.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AdicionaContatoServlet
- */
-@WebServlet("/AdicionaContatoServlet")
-public class AdicionaContatoServlet extends HttpServlet {
+import br.com.caelum.agenda.dao.ContatoDao;
+import br.com.caelum.agenda.modelo.Contato;
+@WebServlet("/add")
+public class AdicionaContatoServlet extends HttpServlet{
+	
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AdicionaContatoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override		
+	protected void service(HttpServletRequest request, HttpServletResponse arg1) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		PrintWriter out = arg1.getWriter();
+		//buscando os parametros no request
+		String nome= request.getParameter("nome");
+		String endereco= request.getParameter("endereco");
+		String email= request.getParameter("email");
+		String dataEmTexto = request.getParameter("dataNascimento");
+		Calendar dataNascimento = null;
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	//monta um objeto contato
+	Contato contato = new Contato();
+	contato.setNome(nome);
+	contato.setEndereco(endereco);
+	contato.setEmail(email);
+	contato.setDataNascimento(dataNascimento);
+
+	//Salva contato
+	ContatoDao dao = new ContatoDao();
+	dao.adiciona(contato);
+
+	//imprime o nome do contato que foi adicionado
+	out.println("<html>");
+	out.println("<body>");
+	out.println("<Contato>"+contato.getNome() +
+			"adicionado com sucesso");
+	out.println("</body>");
+	out.println("</html>");	
 	}
 
 }
